8c0f248377a69c35c173fe1f10a84cf4916b7c7d
Shanoor24 <shanoorfb2403@gmail.com>
2021-08-28 11:48:25 +0530

    nav bar commpleted

--- labgen/src/Components/NavBar/NavBar.jsx
+++ labgen/src/Components/NavBar/NavBar.jsx
@@ -1,41 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { IconButton} from "@material-ui/core";
 import logo from "../../Images/logo.svg"
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    appBar : {
-        backgroundColor: "#6B97B0",
-        height:"14.68vh"
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-
+import {useStyles} from "./style"
 
 
 function NavBar() {
     const classes = useStyles();
+    const [arrow, setArrow] = useState(false);
+
+    const handleArrow = () => {
+      setArrow(!arrow);
+    }
+
+
     return (
         <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar >
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <img src={logo} alt="labgen_logo" style={{width:"19.791vw", height:"9.0027vh", marginTop:"10px"}}/>
+            <img src={logo} alt="labgen_logo" className={classes.logo}/>
           </IconButton>
-                <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '7.89vh', width:"17.8385vw", marginLeft:"37.109vw", marginTop:"10px"}} >
-
+                <Typography component="div" className={classes.user_cont} >
+                    <Typography component="div" className={classes.user_logo} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>
+                      U
+                    </Typography>
+                    <Typography component="div" className={classes.user_title} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>
+                      User Name
+                    </Typography>
+                    <Typography component="div" className={classes.down_arrow} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`, cursor:"pointer"}} onClick={handleArrow}>
+                      {arrow ? "▲" : "▼"}
+                    </Typography>
                 </Typography>
+                {arrow && <Typography component="div" className={classes.user_el_cont} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`, cursor:"pointer", position: "absolute"}}>
+                      <Typography component="div" style={{textAlign: "center"}} className={classes.user_menu}>
+                          Profile        
+                      </Typography>
+                      <Typography component="div" style={{textAlign: "center"}} className={classes.user_menu}>
+                          Sign out       
+                      </Typography>
+                </Typography>}
         </Toolbar>
       </AppBar>
     </div>
---
+++ labgen/src/Components/NavBar/style.js
@@ -0,0 +1,86 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    appBar : {
+        backgroundColor: "#6B97B0",
+        height:"14.68vh"
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    logo: {
+        width:"19.791vw",
+        height:"9.0027vh",
+        marginTop:"1.3850vh"
+    },
+    user_cont: {
+        display: "flex",
+        backgroundColor: '#cfe8fc',
+        height: '7.89vh',
+        width:"17.8385vw",
+        marginLeft:"56.6406vw",
+        marginTop:"1.3850vh",
+        borderRadius: "0.3em"
+    },
+    user_logo: {
+        backgroundColor: "#C4C4C4",
+        width: "2.6692vw",
+        height: "5.4016vh",
+        marginTop: "1.2036vh",
+        marginLeft: "1.0481vw",
+        borderRadius: "50%",
+        paddingTop: "0.55vh",
+        paddingLeft: "0.15vw",
+        boxSizing: "border-box",
+        color: "#000000",
+        fontWeight: "700",
+        fontSize: "1.40vw"
+        // margin: "auto"
+    },
+    user_title: {
+    //    border: "1px solid red",
+       width: "10.5468vw",
+       height: "5.2631vh",
+       color: "#000000",
+       fontStyle: "normal",
+       fontSize: "1.6927vw",
+       fontWeight: "500",
+       marginTop: "1.2036vh"
+    },
+    down_arrow: {
+        width:"2.5781vw",
+        color: "#000000",
+        // border: "1px solid red",
+        height: "5.2631vh",
+        marginTop: "1.3036vh",
+        fontSize: "1.6276vw",
+    },
+    user_el_cont: {
+        border: "1px solid grey",
+        width:"17.8490vw",
+        height: "70px",
+        marginLeft: "1201.5px",
+        marginTop: "138px",
+        backgroundColor: '#cfe8fc',
+        padding : "10px 15px 10px 25px",
+        boxSizing: "border-box",
+        color: "#000000",
+        fontWeight: "700",
+
+    },
+    user_menu: {
+        // paddingLeft: "20px",
+        color: "#000000",
+        fontWeight: "600",
+        marginBottom: "5px",
+        borderRadius: "0.3em"
+    }
+
+  }));
+
