7d5e8b718d8fb66aefbb982a6f4c4f6c317c09ac
Shanoor24 <shanoorfb2403@gmail.com>
2021-08-29 10:44:14 +0530

    patient form work under process

--- labgen/src/Components/PatientForm/PatientFormMain.jsx
+++ labgen/src/Components/PatientForm/PatientFormMain.jsx
@@ -16,7 +16,17 @@ function PatientForm() {
                 <SideBar />
                 <Container maxWidth="lg" className={classes.right_cont}>
                     <Container maxWidth="lg" className={classes.form_main_cont}>
-                        <Typography>New Patient Form</Typography>
+                        <Typography className={classes.form_heading} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>New Patient Entry</Typography>
+                        <Container className={classes.status_bar_cont}>
+                            <Typography className={classes.current_round} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>1</Typography>
+                            <Typography className={classes.empty_bar}></Typography>
+                            <Typography className={classes.empty_circle} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>2</Typography>
+                            <Typography className={classes.empty_bar}></Typography>
+                            <Typography className={classes.empty_circle} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>3</Typography>
+                        </Container>
+                        <Container className={classes.details_cont}>
+                            
+                        </Container>
                     </Container>
                 </Container>
             </Container>
--- labgen/src/Components/PatientForm/style.js
+++ labgen/src/Components/PatientForm/style.js
@@ -27,6 +27,65 @@ export const useStyles = makeStyles((theme) => ({
         height: "650px",
         borderRadius: "1em",
         marginTop: "55px",
+        border: "1px solid #8CD6FF"
+    },
+    form_heading: {
+        border: "2px solid black",
+        padding: "17px 20px 17px 20px",
+        boxSizing: "border-box",
+        width: "650px",
+        borderRadius: "4em",
+        fontWeight: "500",
+        fontSize: "24px",
+        margin: "auto",
+        marginTop: "40px",
+        backgroundColor: "#EADC7D"
+        // textDecoration: "underline"
+    },
+    status_bar_cont: {
+        width: "750px",
+        border: "1px solid #8CD6FF",
+        margin: "auto",
+        marginTop: "30px",
+        display: "flex",
+        padding: "0"
+    },
+    current_round: {
+        width: "25px",
+        height: "25px",
+        border: "5px solid #D6FF8C",
+        borderRadius: "1.5em",
+        fontSize: "17px",
+        fontWeight: "700",
+        color: "#FFFFFF"
+    },
+    empty_bar: {
+        width: "320px",
+        height: "5px",
+        borderBottom: "4px dashed #B1B8C0",
+        marginLeft: "5px",
+        marginTop: "10px"
+    },
+    empty_circle: {
+        width: "20px",
+        height: "20px",
+        backgroundColor: "#B1B8C0",
+        borderRadius: "1.5em",
+        fontSize: "14px",
+        fontWeight: "700",
+        color: "#FFFFFF",
+        marginLeft: "5px",
+        border: "5px solid #B1B8C0",
+        marginTop: "2px"
+    },
+    details_cont: {
+        width: "800px",
+        height: '420px',
+        border: '2px solid #8CD6FF',
+        borderRadius: "1.5em",
+        margin: "auto",
+        marginTop: "20px",
+        backgroundColor: "#B1FFA4"
     }
   }));
 
