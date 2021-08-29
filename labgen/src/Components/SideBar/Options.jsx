dbd8f51bdc68477bba604117a592c463aa8520fb
Shanoor24 <shanoorfb2403@gmail.com>
2021-08-29 20:06:00 +0530

    routes updated

--- labgen/src/Components/SideBar/Options.jsx
+++ labgen/src/Components/SideBar/Options.jsx
@@ -1,9 +1,15 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 import {useStyles} from "./style"
+import {useHistory} from "react-router-dom"
 
 function Options({title, status, handleToggleOption, id, sub_menues}) {
     const classes = useStyles();
+    const history = useHistory();
+
+    const handleRedirect = (path) => {
+        history.push(path)
+    }
     return (
         <div>
             <Typography className={classes.option} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}} >
@@ -12,7 +18,7 @@ function Options({title, status, handleToggleOption, id, sub_menues}) {
                     <Typography className={classes.option_arrow} onClick={() => handleToggleOption(id)} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>{status ? "▼" : "▲"}</Typography>
                 </Typography>
                 {status && <Typography className={classes.sub_menues} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>
-                    {sub_menues.map((el) => <Typography className={classes.sub_menues_title} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>{`${el.title} ►`}</Typography>)}
+                    {sub_menues.map((el) => <Typography className={classes.sub_menues_title} onClick={() => handleRedirect(el.path)} style={{fontFamily: `"M PLUS Rounded 1c", "sans-serif"`}}>{`${el.title} ►`}</Typography>)}
                 </Typography>}
             </Typography>
         </div>
